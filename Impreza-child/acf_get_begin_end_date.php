54b27fcc5fee74f33a7e760964d0ff9f72e2dfa7
Vishal Singh <vishal.singh@netmente.com>
2025-04-03 11:10:58 +0530

    delete all the changes

--- Impreza-child/acf_get_begin_end_date.php
+++
@@ -1,8 +0,0 @@
-<?php
-function get_acf_begin_end_date( $atts ) {
-
-  $field = get_field('begin_date').' - '.get_field('end_date');
-
-  return $field;
-
-}
--- Impreza-child/test.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-echo "---". esc_url( get_stylesheet_directory_uri()) ."\n";
-
-?>
